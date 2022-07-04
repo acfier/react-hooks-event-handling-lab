21777999ad7dd283f27f3aef9c12656945491dac
acfier <acfier@gmail.com>
2022-07-04 13:14:25 -0400

    commit tree a08f0a108e943133fbd81c1bdb2da57197e70a29

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
 // Code EyesOnMe Component Here
+import React from "react"; 
+
+function EyesOnMe () {
+    function handleBlur(event) {
+        console.log("Hey! Eyes on me!");
+      }
+      function handleFocus(event) {
+        console.log("Good!"); 
+      }
+    
+      return (
+        <div>
+          <button onBlur={() => handleBlur()} onFocus={() => handleFocus()} >Eyes on me</button>
+        </div>
+      );
+}
+
+
+
+
+export default EyesOnMe; 
